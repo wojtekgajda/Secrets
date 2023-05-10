4f9825be3adbe31dd9a5382878ea00ef2b90818f
Wojtek Gajda <wojciechgajda@yahoo.com>
2023-05-10 14:49:11 +0200

    hashing passwords

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const ejs = require('ejs');
 const mongoose = require('mongoose');
 const app = express();
 const encrypt = require('mongoose-encryption');
-
+const md5 = require('md5')
 
 
 app.use(express.static('public'));
@@ -20,8 +20,6 @@ const userSchema = new mongoose.Schema({
 })
 
 // ------------- encrypting DB ----------------
-
-userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields:['password']})
 
 
 const User = new mongoose.model('User', userSchema, 'users')
@@ -38,7 +36,7 @@ app.get('/login', (req, res) => {
 
 app.post('/login', (req, res) => {
     const username = req.body.username
-    const password = req.body.password
+    const password = md5(req.body.password)
     console.log(username)
     console.log(password)
 
@@ -65,7 +63,7 @@ app.get('/register', (req, res) => {
 app.post('/register', (req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
     newUser.save().then(() => {
         console.log('New user registered')
